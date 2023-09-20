eaaac6eb70e687c2c7d9bf62a0a3bd3ae1324b7e
Maksimchik36 <Peltek1985@gmail.com>
2023-09-20 14:39:24 +0300

    added login page, changed next-auth route

--- src/app/(pages)/dashboard/(auth)/login/page.jsx
+++ src/app/(pages)/dashboard/(auth)/login/page.jsx
@@ -1,16 +1,58 @@
 "use client";
-import React from 'react';
-import { signIn } from 'next-auth/react';
+import React from "react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.scss";
 
 
-const LoginPage = () => {
-    return (
-        <div>
-            <h1>Login Page</h1>
-            <button onClick={() => signIn('google')}>Login with Google</button>
-        </div>
-    )
-}
+
+const Login = () => {
+    // дает данные о пользователе - data, залогинен он или нет - status
+    const session = useSession();
+    // console.log(session) // { "data": null,  "status": "unauthenticated"}
+
+    const router = useRouter();
+
+    if (session.status === "loading") {
+        return <p>Loading...</p>
+    }
+
+    // перенаправляет на другую страницу за счет роутера
+    if (session.status === "authenticated") {
+        router?.push("/dashboard");
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+        signIn("credentials", { email, password });
+    }
+
+    return <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+
+            <input
+                type="email"
+                placeholder="Petrov@gmail.com"
+                className={styles.input}
+                required
+            />
+
+            <input
+                type="password"
+                placeholder="IvanP12345"
+                className={styles.input}
+                required
+            />
+
+            <button type="submit" className={styles.registerBtn}>Login</button>
+        </form>
+
+        <button onClick={() => signIn("google")} className={styles.loginBtn}>Login with Google</button>
+    </div>;
+};
 
 
-export default LoginPage;
+export default Login;
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,52 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connect from "@/utils/db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
 
 
 const handler = NextAuth({
     providers: [
+        // при использовании провайдеров, в которых заранее нужно прописать разрешения
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
+        // при использовании различных провайдеров в реальном времени
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            async authorize(credentials) {
+                await connect();
+
+                try {
+                    // ищет пользователя по имэйлу в БД
+                    const user = await User.findOne({ email: credentials.email })
+
+                    if (user) {
+                        const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+
+                        if (isPasswordCorrect) {
+                            return user;
+                        } else {
+                            throw new Error("Wrong Credentials.");
+                        }
+
+                    } else {
+                        throw new Error("User not found.");
+                    }
+
+                } catch (err) {
+                    throw new Error(err);
+                }
+            }
+        })
     ],
+    pages: {
+        // про ошибке при логине переходит на указанную ниже страницу (в нашем случае остается на той же)
+        error: "/dashboard/login"
+    },
 })
 
 
