dd56d50381f1bb549693004f9b57a0a4729270ad
Maksimchik36 <Peltek1985@gmail.com>
2023-09-21 14:07:40 +0300

    added cloudinary to nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+const nextConfig = {
+    images: {
+        domains: ["res.cloudinary.com"],
+    },
+}
+
+
+module.exports = nextConfig;
