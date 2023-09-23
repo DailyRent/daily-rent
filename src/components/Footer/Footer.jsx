a23d9f95061a08fea8193d1fd234f382adfe9119
Nataliia <natalabojko@gmail.com>
2023-09-23 22:02:38 +0300

    abracadabra

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-// import styles from "./Footer.module.scss";
-// import CallBtn from "../CallBtn/CallBtn";
-// import OrderBtn from "../OrderBtn/OrderBtn";
-// import Navigation from "../Navigation/Navigation";
-// import Logo from "../Logo/Logo";
 
-// import SocialLinks from "../SocialLinks/SocialLinks";
-// import Link from "next/link";
 import MobileFooter from "./MobileFooter/MobileFooter";
 import TabletFooter from "./TabletFooter/TabletFooter";
 
