c2dc39ca63e41b5f70e2257513b3d64058c444a9
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2023-09-23 17:13:31 +0300

    change sprite and add btn

--- src/app/(pages)/apartments/rooms/1/layout.js
+++ src/app/(pages)/apartments/rooms/1/layout.js
@@ -4,7 +4,24 @@ export default function OneRoomsLayout({ children }) {
   return (
     <section className={styles.container}>
       <h1 className={styles.visuallyHidden}>OneRooms Appartments</h1>
-      <p>filter</p>
+      <div className={styles.oneRoomBox}>
+        <div className={styles.backContainer}>
+          <button type="button" className={styles.backBtnContainer}>
+            <svg className={styles.backSvg}>
+              <use href="/sprite.svg#icon-arrow-left-circle" />
+            </svg>
+          </button>
+          <p className={styles.text}>Головна / Апартаменти / Lorem ipsum </p>
+        </div>
+        <div className={styles.filterContainer}>
+          <button type="button" className={styles.filterBtnContainer}>
+            <svg className={styles.filterSvg}>
+              <use href="/sprite.svg#icon-sliders" />
+            </svg>
+          </button>
+          <p className={styles.filter}>Filter</p>
+        </div>
+      </div>
       <div>{children}</div>
     </section>
   );
