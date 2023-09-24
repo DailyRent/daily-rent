a13fd98ecc28959f1b1c4e3a07df9788c044c0f9
Nataliia <natalabojko@gmail.com>
2023-09-24 16:40:58 +0300

    OrderBtn->Button

--- src/app/(pages)/apartments/page.jsx
+++ src/app/(pages)/apartments/page.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./page.module.scss";
 import ApartItem from "@/components/ApartItem/ApartItem";
-import OrderBtn from "@/components/OrderBtn/OrderBtn";
+// import OrderBtn from "@/components/OrderBtn/OrderBtn";
 import CallBtn from "@/components/CallBtn/CallBtn";
 import Link from "next/link";
 import Filter from "@/components/Filter/Filter";
+import Button from "@/components/Button/Button";
 
 // async function getData() {
 //   const result = await fetch(`${process.env.NEXTAUTH_URL}/api/apartments`, { cache: "no-store" })
@@ -32,8 +33,9 @@ const Apartments = () => {
       {/* <Link href="/apartments/rooms" style={{ backgroundColor: "green" }}>
         Move to page for choose quantity of rooms
       </Link> */}
-      {/* <OrderBtn />
-      <CallBtn /> */}
+      {/* <OrderBtn /> */}
+      <Button />
+      <CallBtn />
     </div>
   );
 };
