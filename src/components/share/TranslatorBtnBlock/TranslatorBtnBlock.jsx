210fac105ce594ac49c86bc3c2b76c8e462d6d23
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2023-09-26 23:17:25 +0300

    added translator button

---
+++ src/components/share/TranslatorBtnBlock/TranslatorBtnBlock.jsx
@@ -0,0 +1,95 @@
+"use client"
+import React, { useState, useEffect, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import styles from './TranslatorBtnBlock.module.scss';
+
+const options = [
+  {
+    id: 1,
+    label: 'EN',
+    value: 'en',
+  },
+  {
+    id: 2,
+    label: 'UA',
+    value: 'ua',
+  },
+];
+
+const TranslatorBtnBlock = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [language, setLanguage] = useState()
+  const { i18n } = useTranslation();
+  const rootEl = useRef(null);
+  
+  useEffect(() => {
+    setLanguage(localStorage.getItem('whatLanguage'));
+  }, [])
+  
+  const changeLanguage = e => {
+    const whatLanguage = e.target.value;
+    localStorage.setItem('whatLanguage', whatLanguage);
+    const language = localStorage.getItem('whatLanguage');
+    setLanguage(language)
+    setIsOpen(false);
+    
+    i18n.changeLanguage(language);
+  };
+
+  useEffect(() => {
+    const onClick = e => rootEl.current.contains(e.target) || setIsOpen(false);
+    document.addEventListener('click', onClick);
+    return () => document.removeEventListener('click', onClick);
+  }, []);
+
+  return (
+    <div ref={rootEl}>
+      <div className={styles.dropdown}>
+        <button
+          className={styles.dropBtn}
+          type="button"
+          onClick={e => {
+            setIsOpen(prev => !prev);
+          }}
+        >
+          {(typeof window !=="undefined" && typeof language !=="undefined") && (language || 'UA')}
+          
+          <svg
+            className={
+              isOpen ? styles.svg + ' ' + styles.svgActive : styles.svg
+            }
+          >
+            <use href="/sprite.svg#icon-chevron-down" />
+          </svg>
+        </button>
+        {isOpen && (
+          <div
+            className={
+              isOpen
+                ? styles.dropdownContent + ' ' + styles.dropdownContentActive
+                : styles.dropdownContent
+            }
+          >
+            {options.map(({ id, label, value }) => (
+              <div key={id} className={styles.dropdownBlock}>
+                <label htmlFor={value} className={styles.label}>
+                  {label}
+                  <input
+                    onClick={e => changeLanguage(e)}
+                    type="text"
+                    id={value}
+                    name={value}
+                    defaultValue={value}
+                    className={styles.input}
+                  />
+                </label>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TranslatorBtnBlock;
