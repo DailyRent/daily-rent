fbd02e483d1a2d65625be0d7f8f7acfe9fcec19d
Maksimchik36 <Peltek1985@gmail.com>
2023-09-27 20:51:43 +0300

    added POST request for apartment and functions for form

--- src/app/(pages)/dashboard/page.jsx
+++ src/app/(pages)/dashboard/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './page.module.scss';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
@@ -9,7 +9,8 @@ import Image from 'next/image';
 
 const Dashboard = () => {
     const session = useSession();
-    // console.log("session", session);
+    const [roomsQuantityValue, setRoomsQuantityValue] = useState("");
+    const [amenitiesValues, setAmenitiesValues] = useState([]);
 
     const fetcher = (...args) => fetch(...args).then(res => res.json())
     const { data, mutate, error, isLoading } = useSWR('/api/apartments', fetcher);
@@ -28,8 +29,67 @@ const Dashboard = () => {
         router.push("/")
     }
 
+    const changeRoomsQuantity = (e) => {
+        setRoomsQuantityValue(e.target.value);
+    }
+
+    const changeAmenities = (e) => {
+        // проверяет есть ли квартира в массиве 
+        const isAmenityIn = amenitiesValues.find(item => item === e.target.value);
+        if (isAmenityIn) {
+            // если есть - она удаляется и создается новый массив, который далее сохраняется
+            const newArr = amenitiesValues.filter(item => item !== e.target.value)
+            setAmenitiesValues(newArr);
+        } else {
+            // если квартиры нет - добавляется в массив
+            const newArray = [...amenitiesValues, e.target.value];
+            setAmenitiesValues(newArray);
+        };
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("e.target", e.target);
+        const objNumber = e.target[0].value;
+        const top = e.target[1].value;
+        const titleImg = e.target[2].value;
+        const imgs = e.target[3].value;
+        const address = e.target[4].value;
+        const flatNumber = e.target[5].value;
+        const googleMapLocation = e.target[6].value;
+        const price = e.target[11].value;
+        const roomsQuantity = roomsQuantityValue;
+        const bookingUrl = e.target[12].value;
+        const amenities = amenitiesValues;
+        const description = e.target[14].value;
+
+        try {
+            await fetch("/api/apartment", {
+                method: "POST",
+                body: JSON.stringify({
+                    objNumber,
+                    top,
+                    titleImg,
+                    imgs,
+                    address,
+                    flatNumber,
+                    googleMapLocation,
+                    price,
+                    roomsQuantity,
+                    bookingUrl,
+                    amenities,
+                    description
+                })
+            })
+            // автоматически обновляет страницу при изменении кол-ва карточек
+            mutate();
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     if (session.status === "authenticated" && session.data.user.email === process.env.NEXT_PUBLIC_ADMIN) {
+
         return <div className={styles.container}>
             <div className={styles.apartments}>
                 {isLoading
@@ -67,9 +127,7 @@ const Dashboard = () => {
                         </div>))}
             </div>
 
-            <form className={styles.new}
-            // onSubmit={handleSubmit}
-            >
+            <form className={styles.new} onSubmit={handleSubmit}>
                 <h1>Додавання нового обʼєкту</h1>
                 <input type='text' placeholder="Номер обʼєкту" className={styles.input} />
                 <label htmlFor="Top" className={styles.top}>
@@ -82,58 +140,58 @@ const Dashboard = () => {
                 <input type='text' placeholder='Місцезнаходження' className={styles.input} />
                 <input type='text' placeholder='Ціна' className={styles.input} />
                 <fieldset className={styles.roomsQuantity}><legend>Кількість кімнат:</legend>
-                    <input type="radio" id="oneRoom" name="roomsQuantity" value="1" />
+                    <input type="radio" id="oneRoom" name="roomsQuantity" value="1" onChange={changeRoomsQuantity} />
                     <label htmlFor="oneRoom">1</label>
-                    <input type="radio" id="twoRooms" name="roomsQuantity" value="2" />
+                    <input type="radio" id="twoRooms" name="roomsQuantity" value="2" onChange={changeRoomsQuantity} />
                     <label htmlFor="twoRooms">2</label>
-                    <input type="radio" id="threeRooms" name="roomsQuantity" value="3" />
+                    <input type="radio" id="threeRooms" name="roomsQuantity" value="3" onChange={changeRoomsQuantity} />
                     <label htmlFor="threeRooms">3</label>
                 </fieldset>
                 <input type='text' placeholder='bookingUrl' className={styles.input} />
                 <fieldset className={styles.amenities}><legend>Додатковий комфорт:</legend>
 
                     <label htmlFor="airCond">
-                        <input type="checkbox" id="airCond" name="airCond" value="Кондиціонер" />
+                        <input type="checkbox" id="airCond" name="airCond" value="Кондиціонер" onChange={changeAmenities} />
                         Кондиціонер
                     </label>
                     <label htmlFor="smartTV">
-                        <input type="checkbox" id="smartTV" name="smartTV" value="СмартТВ" />
+                        <input type="checkbox" id="smartTV" name="smartTV" value="СмартТВ" onChange={changeAmenities} />
                         СмартТВ
                     </label>
                     <label htmlFor="bath">
-                        <input type="checkbox" id="bath" name="bath" value="Ванна" />
+                        <input type="checkbox" id="bath" name="bath" value="Ванна" onChange={changeAmenities} />
                         Ванна
                     </label>
                     <label htmlFor="shower">
-                        <input type="checkbox" id="shower" name="shower" value="Душ" />
+                        <input type="checkbox" id="shower" name="shower" value="Душ" onChange={changeAmenities} />
                         Душ
                     </label>
                     <label htmlFor="jacuzzi">
-                        <input type="checkbox" id="jacuzzi" name="jacuzzi" value="Джакузі" />
+                        <input type="checkbox" id="jacuzzi" name="jacuzzi" value="Джакузі" onChange={changeAmenities} />
                         Джакузі
                     </label>
                     <label htmlFor="microwave">
-                        <input type="checkbox" id="microwave" name="microwave" value="Мікрохвильова піч" />
+                        <input type="checkbox" id="microwave" name="microwave" value="Мікрохвильова піч" onChange={changeAmenities} />
                         Мікрохвильова піч
                     </label>
                     <label htmlFor="washingMachine">
-                        <input type="checkbox" id="washingMachine" name="washingMachine" value="Пральна машина" />
+                        <input type="checkbox" id="washingMachine" name="washingMachine" value="Пральна машина" onChange={changeAmenities} />
                         Пральна машина
                     </label>
                     <label htmlFor="balcony">
-                        <input type="checkbox" id="balcony" name="balcony" value="Балкон" />
+                        <input type="checkbox" id="balcony" name="balcony" value="Балкон" onChange={changeAmenities} />
                         Балкон
                     </label>
                     <label htmlFor="boiler">
-                        <input type="checkbox" id="boiler" name="boiler" value="Бойлер" />
+                        <input type="checkbox" id="boiler" name="boiler" value="Бойлер" onChange={changeAmenities} />
                         Бойлер
                     </label>
                     <label htmlFor="waterHeater">
-                        <input type="checkbox" id="waterHeater" name="waterHeater" value="Водонагрівач" />
+                        <input type="checkbox" id="waterHeater" name="waterHeater" value="Водонагрівач" onChange={changeAmenities} />
                         Водонагрівач
                     </label>
                     <label htmlFor="parking">
-                        <input type="checkbox" id="parking" name="parking" value="Паркінг" />
+                        <input type="checkbox" id="parking" name="parking" value="Паркінг" onChange={changeAmenities} />
                         Паркінг
                     </label>
                 </fieldset>
