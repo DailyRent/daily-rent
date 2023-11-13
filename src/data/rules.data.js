6bd1f630e9a5fb8e1327a5d4497087d8ec92a099
Maksym Padalka <sheihmaks@gmail.com>
2023-11-13 19:50:02 +0100

    Rules

---
+++ src/data/rules.data.js
@@ -0,0 +1,118 @@
+export const RulesInApartment = [
+    {
+      id: 1,
+      ruleEN: 'It is necessary to maintain silence and public order in the apartment.',
+      rule: 'Необхідно зберігати тишу та громадський порядок в квартирі.',
+    },
+    {
+      id: 2,
+      ruleEN: 'In case of damage to the property located in the apartment, the tenant compensate the landlord for the market value of the damaged property.',
+      rule: 'У випадку пошкодження майна, яке знаходиться в квартирі, орендар компенсує орендодавцю ринкову вартість пошкодженого майна.',
+    },
+    {
+      id: 3,
+      ruleEN: 'Check-in takes place from 13.00. Check-out takes place at 12.00.',
+      rule: 'Заселення відбувається з 13.00. Виселення відбувається о 12.00.',
+    },
+    {
+      id: 4,
+      ruleEN:'Check-in time is 12.00, regardless of the time of arrival.',
+      rule: 'Розрахунковий час – 12.00, незалежно від часу заселення.',
+    },
+    {
+      id: 5,
+      ruleEN: "The administration is not responsible for the safety of the tenant's belongings. of the tenant.",
+      rule: 'Адміністрація не несе відповідальності за збереження речей орендаря.',
+    },
+    {
+      id: 6,
+      ruleEN: 'The administration is not responsible for the operation of city utilities (water, electricity, gas, heating).',
+      rule: 'Адміністрація не несе відповідальності за роботу міських комунікацій (відключення води, світла, газу, опалення).',
+    },
+    {
+      id: 7,
+      ruleEN: 'The rent is not refundable if you decide to leave the rented premises earlier than the scheduled date.',
+      rule: 'Орендна плата за проживання не повертається, якщо Ви вирішите залишити орендоване приміщення раніше запланованої дати.',
+    },
+    {
+      id: 8,
+      ruleEN: 'Prepayment for accommodation is non-refundable.',
+      rule: 'Передплата за проживання не повертається.',
+    },
+    {
+      id: 9,
+      ruleEN: 'The number of people who can stay in an apartment-hotel cannot exceed 2 people. If there are more people in the apartment than the agreed number, the administration has the right to immediately evict the tenant without a refund.',
+      rule: 'Кількість людей, які можуть перебувати у квартирі-готелі, не може перевищувати 2-х осіб. У разі перебування у квартирі людей, більше домовленої кількості, адміністрація має право негайно виселити орендаря без повернення грошей за проживання.',
+    },
+  ];
+
+  export const Prohibited=[
+    {
+        id: 10,
+        ruleEN: "Smoking in the apartment-hotel. In case of smoking in the apartment-hotel, the tenant is obliged to pay a fine in the amount of the accommodation fee for 1 day. SMOKING IS PROHIBITED.",
+        rule: "Палити у приміщенні квартири-готелі. У разі паління у приміщенні квартири-готелю орендар зобов'язується сплатити штраф у розмірі плати за проживання за 1 добу. ПАЛИТИ ЗАБОРОНЕНО.",
+      },
+      {
+        id: 11,
+        ruleEN: 'Disturb silence and public order.',
+        rule: 'Порушувати тишу та громадський порядок.',
+      },
+      {
+        id: 12,
+        ruleEN: 'To hold celebrations, meetings, etc. in a rented apartment-hotel, parties, etc.',
+        rule: 'Проводити в орендованій квартирі-готелі святкування, зустрічі, гуляння тощо.',
+      },
+      {
+        id: 13,
+        ruleEN:'To place unauthorized people in the rented apartment-hotel, more than than the agreed number.',
+        rule: 'Поміщати в квартиру-готель, що орендується, сторонніх людей, більш домовленої кількості.',
+      },
+      {
+        id: 14,
+        ruleEN: 'Leave strangers in the rented apartment-hotel, give them the keys to the apartment, passwords to the "Internet".',
+        rule: 'Залишати в квартирі-готелі, що орендується, сторонніх людей, передавати їм ключі від квартири, паролі доступу до мережі «Інтернет».',
+      },
+      {
+        id: 15,
+        ruleEN: 'The apartment-hotel is strictly not rented for events, groups of people, companies, for celebrating any celebrations and other meetings involving more than 2 people. It is forbidden to invite guests to the to the apartment-hotel for tea and other meetings.',
+        rule: 'Квартира-готель категорично не здається для заходів, групам осіб, компаніям, для святкування будь-яких урочистостей та інших зустрічей за участю більше 2-х осіб. Забороняється запрошувати у квартиру-готель гостей на чай та інші зустрічі.',
+      },
+  ]
+
+  export const Eviction=[
+    {
+        id: 16,
+        ruleEN: "Violation of these rules.",
+        rule: "Порушення цих правил.",
+      },
+      {
+        id: 17,
+        ruleEN: 'Smoking in the apartment-hotel.',
+        rule: 'Паління у квартирі-готелі.',
+      },
+      {
+        id: 18,
+        ruleEN: 'Violation of public order rules in the apartment-hotel.',
+        rule: 'Порушення правил громадського порядку у квартирі-готелі.',
+      },
+      {
+        id: 19,
+        ruleEN:'Claims from neighbors living nearby.',
+        rule: "Пред'явлення претензій з боку сусідів, що проживають поруч.",
+      },
+      {
+        id: 20,
+        ruleEN: 'Having more than the agreed number of people in the apartment.',
+        rule: 'Перебування у квартирі людей, більше домовленої кількості.',
+      },
+      {
+        id: 21,
+        ruleEN: 'Holding celebrations, meetings, parties, etc. in the apartment-hotel.',
+        rule: 'Проведення у квартирі-готелі святкувань, зустрічей, гулянь тощо.',
+      },
+      {
+        id: 16,
+        ruleEN: 'Causing significant material damage to the lessor.',
+        rule: 'Заподіяння значних матеріальних збитків орендодавцю.',
+      },
+  ]
