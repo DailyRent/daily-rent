906eac4cacb89068bf9527537bb72dca0b7b4758
Maksym Padalka <sheihmaks@gmail.com>
2023-11-13 20:12:12 +0100

    Update page.jsx

--- src/app/(pages)/rules/page.jsx
+++ src/app/(pages)/rules/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import styles from "./page.module.scss";
+import { RulesInApartment, Prohibited, Eviction, currentLanguages } from "@/data";
 import { useTranslation } from "react-i18next";
 import { useState, useEffect } from "react";
 // import ButtonToBack from "@/components/share/ButtonToBack/ButtonToBack";
@@ -10,7 +11,7 @@ import seoStyles from "@/app/seoStyles.module.css";
 const RulesPage = () => {
   // const router = useRouter();
   const { t } = useTranslation();
-
+  const {i18n}= useTranslation()
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -33,14 +34,17 @@ const RulesPage = () => {
         )}
       </div>
 
-      <h2 className={styles.rulesListItem}>правила проживання:</h2>
+      {!isLoading && <><h2 className={styles.rulesListItem}>{t("RulesPage.MainTitle")}</h2>
       <ul className={styles.rulesList}>
         <li>
           <h3 className={styles.decimalListTitle}>
-            Правила поведінки в квартирі:
+          {t("RulesPage.TitleSection1")}
           </h3>
           <ol className={styles.decimalList}>
-            <li>Необхідно зберігати тишу та громадський порядок в квартирі.</li>
+            {RulesInApartment.map(({id,rule,ruleEN})=>{
+              return (<li key={id}>{i18n.language===currentLanguages.EN ? ruleEN : rule  }</li>)
+            })}
+            {/* <li>Необхідно зберігати тишу та громадський порядок в квартирі.</li>
             <li>
               У випадку пошкодження майна, яке знаходиться в квартирі, орендар
               компенсує орендодавцю ринкову вартість пошкодженого майна. 
@@ -67,15 +71,18 @@ const RulesPage = () => {
               перевищувати 2-х осіб. У разі перебування у квартирі людей, більше
               домовленої кількості, адміністрація має право негайно виселити
               орендаря без повернення грошей за проживання.
-            </li>
+            </li> */}
           </ol>
         </li>
         <li>
           <h3 className={styles.decimalListTitle}>
-            Категорично забороняється:
+          {t("RulesPage.TitleSection2")}
           </h3>
           <ol className={styles.decimalList}>
-            <li>
+            {Prohibited.map(({id,rule,ruleEN})=>{
+              return (<li key={id}>{i18n.language===currentLanguages.EN ? ruleEN : rule  }</li>)
+            })}
+            {/* <li>
               Палити у приміщенні квартири-готелі. У разі паління у приміщенні
               квартири-готелю орендар зобов&apos;язується сплатити штраф у
               розмірі плати за проживання за 1 добу. ПАЛИТИ ЗАБОРОНЕНО.
@@ -99,16 +106,18 @@ const RulesPage = () => {
               компаніям, для святкування будь-яких урочистостей та інших
               зустрічей за участю більше 2-х осіб. Забороняється запрошувати у
               квартиру-готель гостей на чай та інші зустрічі.
-            </li>
+            </li> */}
           </ol>
         </li>
         <li>
           <h3 className={styles.decimalListTitle}>
-            Адміністрація має право негайно виселити орендаря без повер - нення
-            грошей за проживання, у разі:
+          {t("RulesPage.TitleSection3")}
           </h3>
           <ol className={styles.decimalList}>
-            <li>Порушення цих правил.</li>
+            {Eviction.map(({id,rule,ruleEN})=>{
+              return (<li key={id}>{i18n.language===currentLanguages.EN ? ruleEN : rule  }</li>)
+            })}
+            {/* <li>Порушення цих правил.</li>
             <li>Паління у квартирі-готелі.</li>
             <li>Порушення правил громадського порядку у квартирі-готелі.</li>
             <li>
@@ -118,10 +127,10 @@ const RulesPage = () => {
             <li>
               Проведення у квартирі-готелі святкувань, зустрічей, гулянь тощо.
             </li>
-            <li>Заподіяння значних матеріальних збитків орендодавцю.</li>
+            <li>Заподіяння значних матеріальних збитків орендодавцю.</li> */}
           </ol>
         </li>
-      </ul>
+      </ul></>}
     </section>
   );
 };
