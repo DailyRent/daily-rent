50f8aa560a60a85375cda31b93749e9fcf84f683
Maksimchik36 <Peltek1985@gmail.com>
2023-12-15 00:44:30 +0200

    added bedsQuantity to amenities in apartId

--- src/components/ApartIdItem/Amenities/Amenities.jsx
+++ src/components/ApartIdItem/Amenities/Amenities.jsx
@@ -1,3 +1,52 @@
+// import { amenities } from "@/data/amenities.data";
+// import { currentLanguages } from "@/data/languages.data";
+// import Image from "next/image";
+// import { useTranslation } from "react-i18next";
+// import styles from "./Amenities.module.scss";
+
+// const Amenities = ({ dataId }) => {
+//   const { i18n } = useTranslation();
+//   const { t } = useTranslation();
+
+//   const matchingAmenities = amenities.filter((amenity) =>
+//     dataId.amenities.includes(amenity.title)
+//   );
+
+//   return (
+//     <div className={styles.propositionContainer}>
+//       <h2 className={styles.propositionTitle}>{t("ApartmentsPage.TextOfDescOptions")} ?</h2>
+//       <ul className={styles.propositionList}>
+//         {matchingAmenities.map((amenity) => (
+//           <li key={amenity.id} className={styles.propositionItem}>
+//             <div className={styles.imgSvgContainer}>
+//               <Image
+//                 src={amenity.img}
+//                 alt="Shower"
+//                 fill={true}
+//                 className={styles.imgSvg}
+//                 sizes="(min-width: 768px) 24px,"
+//               />
+//             </div>
+//             {i18n.language === currentLanguages.EN ? (
+//               <p>{amenity.titleEN}</p>
+//             ) : (
+//               <p>{amenity.title}</p>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Amenities;
+//
+//
+//
+//
+//
+//
+
 import { amenities } from "@/data/amenities.data";
 import { currentLanguages } from "@/data/languages.data";
 import Image from "next/image";
@@ -8,20 +57,30 @@ const Amenities = ({ dataId }) => {
   const { i18n } = useTranslation();
   const { t } = useTranslation();
 
+  const bed = {
+    id: 100,
+    titleEN: dataId.bedsQuantity,
+    title: dataId.bedsQuantity,
+    img: "/webp/Bed.webp",
+  };
+
   const matchingAmenities = amenities.filter((amenity) =>
     dataId.amenities.includes(amenity.title)
   );
 
+  const matchingAmenitiesWithBed = [...matchingAmenities, bed];
   return (
     <div className={styles.propositionContainer}>
-      <h2 className={styles.propositionTitle}>{t("ApartmentsPage.TextOfDescOptions")} ?</h2>
+      <h2 className={styles.propositionTitle}>
+        {t("ApartmentsPage.TextOfDescOptions")} ?
+      </h2>
       <ul className={styles.propositionList}>
-        {matchingAmenities.map((amenity) => (
+        {matchingAmenitiesWithBed.map((amenity) => (
           <li key={amenity.id} className={styles.propositionItem}>
             <div className={styles.imgSvgContainer}>
               <Image
                 src={amenity.img}
-                alt="Shower"
+                alt={amenity.titleEn}
                 fill={true}
                 className={styles.imgSvg}
                 sizes="(min-width: 768px) 24px,"
---
+++ src/data/beds.data.js
@@ -0,0 +1,34 @@
+// import { v4 } from "uuid";
+
+export const beds = [
+  {
+    id: 1,
+    titleEN: "2",
+    title: "2",
+    img: "/webp/Bed.webp",
+  },
+  {
+    id: 2,
+    titleEN: "3",
+    title: "3",
+    img: "/webp/Bed.webp",
+  },
+  {
+    id: 3,
+    titleEN: "4",
+   title: "4",
+    img: "/webp/Bed.webp",
+  },
+  {
+    id: 4,
+    titleEN: "5",
+    title: "5",
+    img: "/webp/Bed.webp",
+  },
+  {
+    id: 5,
+    titleEN: "6",
+    title: "6",
+    img: "/webp/Bed.webp",
+  },
+];
