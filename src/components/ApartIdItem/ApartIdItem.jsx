a61fa6dfa41b3cd25216895af3deae64d39434f4
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2023-12-20 21:43:29 +0200

    add text - apartIdPage and contactPage

--- src/components/ApartIdItem/ApartIdItem.jsx
+++ src/components/ApartIdItem/ApartIdItem.jsx
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
-import { useContext } from "react";
-import { useTranslation } from "react-i18next";
-import OrderBtn from "../OrderBtn/OrderBtn";
-import IsLoading from "../share/IsLoading/IsLoading";
-import Amenities from "./Amenities/Amenities";
-import ApartDataList from "./ApartDataList/ApartDataList";
-import styles from "./ApartIdItem.module.scss";
+import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import OrderBtn from '../OrderBtn/OrderBtn';
+import IsLoading from '../share/IsLoading/IsLoading';
+import Amenities from './Amenities/Amenities';
+import ApartDataList from './ApartDataList/ApartDataList';
+import styles from './ApartIdItem.module.scss';
 // import ApartIdSlider from './ApartIdSlider/ApartIdSlider';
-import ItemSlider from "./ItemSlider/ItemSlider";
-import ApartStar from "./ApartStar/ApartStar";
-import ModalR from "@/components/Modal/Modal";
-import OrderForm from "@/components/OrderForm/OrderForm";
-import { SiteContext } from "@/context/SiteContext";
-import Link from "next/link";
-import seoStyles from "@/app/seoStyles.module.css";
-import useSWR from "swr";
+import ItemSlider from './ItemSlider/ItemSlider';
+import ApartStar from './ApartStar/ApartStar';
+import ModalR from '@/components/Modal/Modal';
+import OrderForm from '@/components/OrderForm/OrderForm';
+import { SiteContext } from '@/context/SiteContext';
+import Link from 'next/link';
+import seoStyles from '@/app/seoStyles.module.css';
+import useSWR from 'swr';
 
 const ApartIdItem = ({ params }) => {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
@@ -39,11 +39,11 @@ const ApartIdItem = ({ params }) => {
         {!isLoading && (
           <span className="textLink">
             <Link href="/" prefetch={false} className="textLinkAnimation">
-              {t("Navigation.MainPage")}
+              {t('Navigation.MainPage')}
             </Link>
             /
             <Link href="/apartments" className="textLinkAnimation">
-              {t("Navigation.Apartments")}
+              {t('Navigation.Apartments')}
             </Link>
             /<span className={styles.active}># {dataId?.objNumber}</span>
           </span>
@@ -70,35 +70,38 @@ const ApartIdItem = ({ params }) => {
       {/* <p className={styles.textInfo}>
         {i18n.language === "ua" ? dataId?.description : dataId?.descriptionEn}
       </p> */}
-      <p className={styles.textInfo}>
-        Lorem ipsum dolor sit amet consectetur. A fermentum venenatis interdum
-        ornare. Augue semper at risus neque maecenas neque potenti eget.
-        Consequat neque in congue vitae rhoncus enim massa placerat. Duis
-        laoreet pulvinar id egestas orci sit gravida. Massa quam arcu at
-        gravida. Egestas arcu urna vulputate morbi. Euismod ut commodo sed
-        augue. In amet in egestas auctor cursus sit. Semper etiam etiam sit
-        diam. Tellus molestie a sit egestas. Amet ultricies arcu nibh est orci
-        in dui. Mauris dignissim lobortis magna odio amet a. Netus faucibus ut
-        est duis. Vehicula ut enim dolor aliquam. Mauris magna sed ultricies
-        dolor ultrices nunc non enim urna. Montes viverra quis malesuada eget.
-        Hendrerit leo massa massa diam adipiscing. Posuere ultrices malesuada
-        vitae bibendum pulvinar. Quam volutpat at ornare gravida nec donec
-        consectetur. Ultrices aliquam ac nec fames purus sed mauris dui. Non a
-        nulla est quis nunc porttitor. Ut amet duis dictum quam ut cras ac. Nisl
-        sollicitudin faucibus eu blandit odio lectus nulla.
-      </p>
-      {/* <div className={styles.contactsMap}>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2515.95985061217!2d34.815601!3d50.905958999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDU0JzIxLjUiTiAzNMKwNDgnNTYuMiJF!5e0!3m2!1suk!2sua!4v1696106361244!5m2!1suk!2sua"
-          // src={dataId?.googleMapLocation}
-          width="600"
-          height="600"
-          allowFullScreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title="Адреса на мапі"
-        ></iframe>
-      </div> */}
+      <div className={styles.textGrid}>
+        <p className={styles.textWelcome}>Ласкаво просимо до DailyRent.</p>
+        <div className={styles.textInfo}>
+          <span>
+            &#8226; Наші просторі квартири вражають комфортом та стилем,
+            ідеально підходять для відпочинку чи робочого перебування у місті
+            Суми.
+          </span>
+          <span>
+            &#8226; Кожна квартира обладнана зручними меблями, повністю
+            укомплектована кухнею, технікою та Wi-Fi.
+          </span>
+          <span>
+            &#8226; Гарантуємо комфорт та стиль для вашого відпочинку чи роботи
+            в місті.
+          </span>
+          <span>
+            &#8226; Локації квартир забезпечують легкий доступ до пам&#39;яток
+            та інфраструктури. Забронюйте подобово за доступною ціною та
+            насолоджуйтеся зручністю житла в центрі міста.
+          </span>
+          <span>
+            &#8226; Виберіть наше житло для оренди та насолоджуйтеся комфортом
+            та стилем. Зробіть своє перебування незабутнім з нашими зручними
+            квартирами.
+          </span>
+          <span>
+            &#8226; Заходьте до нас та насолоджуйтеся комфортом у кожній
+            квартирі на подобову оренду у Сумах.
+          </span>
+        </div>
+      </div>
     </section>
   );
 };
