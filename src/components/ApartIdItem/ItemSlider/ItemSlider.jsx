f0e4e9937a8b2c39cf069b7bd07926134bdc6fd0
Nataliia <natalabojko@gmail.com>
2024-01-02 18:48:07 +0200

    fixed ApartIdItem slider bugs

--- src/components/ApartIdItem/ItemSlider/ItemSlider.jsx
+++ src/components/ApartIdItem/ItemSlider/ItemSlider.jsx
@@ -22,79 +22,85 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./ItemSliderTop.css";
 import "./ItemSliderBottom.css";
+import { useEffect } from "react";
+import { useMemo } from "react";
 
 const ItemSlider = ({ dataId }) => {
+  const [item, setItem] = useState(null);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   const images = dataId?.imgs;
 
-  const allImages = [dataId?.titleImg, ...images];
-  const priorityImg = dataId?.titleImg ? true : false;
+  const allImages = useMemo(
+    () => [dataId?.titleImg, ...images],
+    [dataId, images]
+  );
 
-  const item = allImages.map((item) => {
-    // console.log(item);
-    return (
-      <SwiperSlide key={v4()}>
-        <CldImage
-          src={item}
-          alt="Flat image"
-          fill={true}
-          loading="lazy"
-          sizes="50vw"
-        />
-      </SwiperSlide>
+  const imgPtiority = dataId?.titleImg ? true : false;
+  const imgLoading = dataId?.titleImg ? "eager" : "lazy";
+
+  useEffect(() => {
+    setItem(
+      allImages.map((item) => {
+        return (
+          <SwiperSlide key={v4()}>
+            <CldImage
+              src={item}
+              alt="Flat image"
+              fill={true}
+              loading={imgLoading}
+              sizes="50vw"
+              priority={imgPtiority}
+            />
+          </SwiperSlide>
+        );
+      })
     );
-  });
+  }, [allImages, imgLoading, imgPtiority]);
 
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
     <article className={styles.swiperContainer}>
       <h4 className={seoStyles.titleHidden}>
         Detailed images of the apartment
       </h4>
-      <Swiper
-        loop={true}
-        spaceBetween={10}
-        navigation={true}
-        pagination={{
-          clickable: true,
-          dynamicBullets: true,
-        }}
-        keyboard={{
-          enabled: true,
-        }}
-        // mousewheel={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[
-          FreeMode,
-          Navigation,
-          Pagination,
-          Thumbs,
-          Mousewheel,
-          Keyboard,
-        ]}
-        className="ItemSliderTop"
-      >
-        {item}
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        spaceBetween={16}
-        slidesPerView={3}
-        mousewheel={true}
-        loop={true}
-        freeMode={true}
-        watchSlidesProgress={true}
-        keyboard={{
-          enabled: true,
-        }}
-        // breakpoints={{
-        //   580: { slidesPerView: 4 },
-        //   768: { slidesPerView: 3 },
-        // }}
-        modules={[FreeMode, Navigation, Thumbs, Mousewheel, Keyboard]}
-        className="ItemSliderBottom"
-      >
-        {item}
-      </Swiper>
+      {item && (
+        <Swiper
+          loop={true}
+          spaceBetween={10}
+          navigation={true}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true,
+          }}
+          keyboard={{
+            enabled: true,
+          }}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[FreeMode, Navigation, Pagination, Thumbs, Keyboard]}
+          className="ItemSliderTop"
+        >
+          {item}
+        </Swiper>
+      )}
+
+      {item && (
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={16}
+          slidesPerView={3}
+          mousewheel={true}
+          loop={true}
+          freeMode={true}
+          watchSlidesProgress={true}
+          keyboard={{
+            enabled: true,
+          }}
+          modules={[FreeMode, Navigation, Thumbs, Keyboard, Mousewheel]}
+          className="ItemSliderBottom"
+        >
+          {item}
+        </Swiper>
+      )}
     </article>
   );
 };
